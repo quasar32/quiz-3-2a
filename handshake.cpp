417a481035055d510df9a49401771b33aff513ec
quasar32 <fontejam@tamu.edu>
2023-03-30 18:11:28 -0500

    First

--- handshake.cpp
+++ handshake.cpp
@@ -1,28 +1,52 @@
 #include <iostream>
-// include additional necessary headers
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <unistd.h>
 
-void query(/*add necessary parameters*/) {
-    // Should print: the print number (starting from 0), "SYN", and the three dots "..."
+using namespace std;
+
+bool syn = true, ack = false;
+int count;
+condition_variable cv;
+mutex m;
+
+void query() {
+	for (int i = 0; i < count; i++) { 
+		unique_lock lk(m);
+		cv.wait(lk, [] {return syn; });
+		printf("[%d] SYN ...", i);
+		ack = true;
+		syn = false;
+		lk.unlock();
+		cv.notify_one();
+	}
 }
 
-void response(/*add necessary parameters*/) {
-    // Should print "ACK"
+void response() {
+	for (int i = 0; i < count; i++) {
+		unique_lock lk(m);
+		cv.wait(lk, [] {return ack; }); 
+		printf(" ACK\n");
+		ack = false;
+		syn = true;
+		lk.unlock();
+		cv.notify_one();
+	}
 }
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        printf("Usage: ./handshake <count>\n");
-        exit(1);
-    }
+	if (argc < 2) {
+		printf("Usage: ./handshake <count>\n");
+		exit(1);
+	}
 
-    /**
-     * Steps to follow:
-     * 1. Get the "count" from cmd args
-     * 2. Create necessary synchronization primitive(s)
-     * 3. Create two threads, one for "SYN" and the other for "ACK"
-     * 4. Provide the threads with necessary args
-     * 5. Update the "query" and "response" functions to synchronize the output
-    */
-   
-    return 0;
+	count = atoi(argv[1]); 
+
+	thread tsyn(query);
+	cv.notify_one();
+	thread tack(response);
+	tsyn.join();
+	tack.join();
+	return 0;
 }
